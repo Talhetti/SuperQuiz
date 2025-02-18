a17a658701954e2d38da92bb9ad3011898dca1a1
Alvaro <talhettialvaro@edu.unifil.br>
2025-02-17 23:23:57 -0300

    Adicionando informações para a tela Registrar

--- Registro.php
+++ Registro.php
@@ -10,7 +10,27 @@
 
 <body>
     <div class="containerPrincipal">
-        
+        <div class="imagem-logo">
+            <img src="img/Logo.png" alt="">
+        </div>
+        <div class="Titulo">
+            <h1>REGISTRE-SE</h1>
+        </div>
+        <div class="Form-User">
+            <form action="Registro.php" method="POST">
+                <label for="">Nome: </label>
+                <input type="text">
+                
+                <label for="">Email: </label>
+                <input type="text">
+
+                <label for="">Senha: </label>
+                <input type="password">
+
+                <button type="Submit">Enviar</button>
+            </form>
+        </div>
+    
     </div>
 </body>
 </html>
