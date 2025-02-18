0957007975e8606b96f4fb27de56b1db70e1e48d
Alvaro <talhettialvaro@edu.unifil.br>
2025-02-17 21:56:35 -0300

    tela de regristro pre-pronto

--- Registro.php
+++ Registro.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <title>Registro - Super Quiz</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/Registro.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-    
+    <div class="containerPrincipal">
+        
+    </div>
 </body>
 </html>
